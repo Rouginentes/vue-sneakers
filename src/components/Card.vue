26f87da39e59087a67e6f441513909d6578940ff
Nikita Konovodov <themanul1234@gmail.com>
2025-01-15 02:04:38 +0500

    new_changes

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition"
+  >
+    <img class="cursor-pointer absolute top-8 left-8" src="/like-1.svg" alt="like 1" />
+    <img src="/sneakers/sneakers-1.jpg" alt="sneakers" />
+    <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
+
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Цена:</span>
+        <b>12 999 руб.</b>
+      </div>
+
+      <img src="/plus.svg" alt="" />
+    </div>
+  </div>
+</template>
