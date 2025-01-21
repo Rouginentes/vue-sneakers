b18d05488a07670f96ac233a8f4f70d0b6a3136d
Nikita Konovodov <themanul1234@gmail.com>
2025-01-21 11:23:18 +0500

    finale-result

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <script setup>
+import { ref } from 'vue'
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 import Drawer from './components/Drawer.vue'
+
+const drawerOpen = ref(false)
+
+const closeDrawer = () => {
+  drawerOpen.value = false
+}
+
+const openDrawer = () => {
+  drawerOpen.value = true
+}
 </script>
 
 <template>
-  <Drawer />
+  <Drawer v-if="drawerOpen" />
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none" name="" id="">
+            <option value="">По названию</option>
+            <option value="">По цене(дешевые)</option>
+            <option value="">По цене(дорогие)</option>
+          </select>
+          <div class="relative">
+            <img class="absolute left-4 top-3" src="/search.svg" alt="" />
+            <input
+              class="border border-slate-200 py-2 pr-4 pl-11 rounded-md outline-none focus:border-gray-300"
+              type="text"
+              placeholder="Поиск..."
+            />
+          </div>
+        </div>
+      </div>
 
       <CardList />
     </div>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -3,10 +3,87 @@ import Card from './Card.vue'
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-5">
+  <div class="mt-10 grid grid-cols-4 gap-5">
     <Card
       image-url="./sneakers/sneakers-1.jpg"
       title="Мужские Кроссовки Nike Blazer Mid Suede"
+      :price="12999"
+      :is-added="false"
+      :is-favorite="false"
+    />
+    <Card
+      image-url="./sneakers/sneakers-2.jpg"
+      title="Мужские Кроссовки Nike Air Max 270"
+      :price="12999"
+      :is-added="false"
+      :is-favorite="false"
+    />
+    <Card
+      image-url="./sneakers/sneakers-3.jpg"
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      :price="12999"
+      :is-added="false"
+      :is-favorite="false"
+    />
+    <Card
+      image-url="./sneakers/sneakers-4.jpg"
+      title="Кроссовки Puma X Aka Boku Future Rider"
+      :price="12999"
+      :is-added="false"
+      :is-favorite="false"
+    />
+    <Card
+      image-url="./sneakers/sneakers-5.jpg"
+      title="Мужские Кроссовки Under Armour Curry 8"
+      :price="12999"
+      :is-added="false"
+      :is-favorite="false"
+    />
+    <Card
+      image-url="./sneakers/sneakers-6.jpg"
+      title="Мужские Кроссовки Nike Kyrie 7"
+      :price="12999"
+      :is-added="false"
+      :is-favorite="false"
+    />
+    <Card
+      image-url="./sneakers/sneakers-7.jpg"
+      title="Мужские Кроссовки Jordan Air Jordan 11"
+      :price="12999"
+      :is-added="false"
+      :is-favorite="false"
+    />
+    <Card
+      image-url="./sneakers/sneakers-8.jpg"
+      title="Мужские Кроссовки Nike LeBron XVIII"
+      :price="12999"
+      :is-added="false"
+      :is-favorite="false"
+    />
+    <Card
+      image-url="./sneakers/sneakers-9.jpg"
+      title="Мужские Кроссовки Nike Lebron XVIII Low"
+      :price="12999"
+      :is-added="false"
+      :is-favorite="false"
+    />
+    <Card
+      image-url="./sneakers/sneakers-10.jpg"
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      :price="12999"
+      :is-added="false"
+      :is-favorite="false"
+    />
+    <Card
+      image-url="./sneakers/sneakers-11.jpg"
+      title="Кроссовки Puma X Aka Boku Future Rider"
+      :price="12999"
+      :is-added="false"
+      :is-favorite="false"
+    />
+    <Card
+      image-url="./sneakers/sneakers-12.jpg"
+      title="Мужские Кроссовки Nike Kyrie Flytrap IV"
       :price="12999"
       :is-added="false"
       :is-favorite="false"
